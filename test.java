4a362312808fa365178a1845989b61a5c285e6bc
Aiswarya1707 <83941584+Aiswarya1707@users.noreply.github.com>
2021-05-11 14:04:34 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IfDemo {
+	
+	public static void main(String[] args) {
+		
+		int num;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter any number = ");
+		num=s.nextInt();
+		if(num==20)
+		{
+			System.out.println("Are equal");
+		}
+		else
+			System.out.println("Are not equal");
+		
+		
+	}
+	
+
+}
